e40158724e6ade011562f9f20ba477588776a32a
Ezz <ezmarie97@gmail.com>
2026-03-31 21:41:25 +0300

    Add function to create a new branch

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 import fs from "node:fs";
 
+let curRepo = "x";
+
 function createRepo(repoName) {
   let res = createDir(repoName);
 
   if (res) {
     console.log(`Repository ${repoName} created Successfully!`);
+  }
+
+  if (res === false) {
+    console.log("Another repository with same name already exists.");
+  }
+
+  if (res === undefined) {
+    console.error("Failed to create new directory");
+  }
+}
+
+function createBranch(branchName) {
+  let res = createDir(`${curRepo}/${branchName}`);
+
+  if (res) {
+    console.log(`Repository ${branchName} created Successfully!`);
   }
 
   if (res === false) {
@@ -38,6 +56,9 @@ console.log(`Action: ${action}, Value: ${value}`);
 switch (action) {
   case "create-repo":
     createRepo(value);
+    break;
+  case "create-branch":
+    createBranch(value);
     break;
   default:
     console.log("Unknown action, Try again");
